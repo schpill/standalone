71a0cca34bdcfa759e3d1ebb8d5e7330be25c46d
Gérald Plusquellec <gplusquellec@gmail.com>
2016-01-25 20:12:28 +0100

    cms

--- facades.php
+++ facades.php
@@ -1,16 +1,6 @@
 <?php
     namespace Thin;
 
-    Alias::facade('KVES',           'Es',                   'Kvs');
-    Alias::facade('KVS',            'Mysql',                'Kvs');
-    Alias::facade('KVLS',           'Lite',                 'Kvs');
-    Alias::facade('Io',             'Client',               'ElephantIO');
-    Alias::facade('ZLCache',        'Cache',                'Zelift');
-    Alias::facade('ZLLog',          'Log',                  'Zelift');
-    Alias::facade('ZLModel',        'Model',                'Zelift');
-    Alias::facade('ZLFile',         'File',                 'Zelift');
-    Alias::facade('Sess',           'Sessionstore',         'Thin');
-    Alias::facade('Em',             'Entity',               'Dbjson');
     Alias::facade('Model',          'Entity',               'Dbredis');
     Alias::facade('Raw',            'Entity',               'Raw');
     Alias::facade('N0sql',          'Entity',               'Nosql');
@@ -23,21 +13,6 @@
     Alias::facade('Q',              'Query',                'Dbredis');
     Alias::facade('R',              'Entity',               'Dbredis');
     Alias::facade('Red',            'Db',                   'Dbredis');
-    Alias::facade('Ev',             'Manager',              'Phalcon\\Events');
-    Alias::facade('L',              'File',                 'Phalcon\\Logger\\Adapter');
-    Alias::facade('Res',            'Http\\Response',       'Phalcon');
-    Alias::facade('MVCController',  'Mvc\\Controller',      'Phalcon');
-    Alias::facade('MVCApp',         'Mvc\\Application',     'Phalcon');
-    Alias::facade('MVCView',        'Mvc\\View',            'Phalcon');
-    Alias::facade('C',              'Mvc\\Collection',      'Phalcon');
-    Alias::facade('AclModel',       'Acl',                  'Phalcon');
-    Alias::facade('AppEx',          'Exception',            'Phalcon');
-    Alias::facade('AclDb',          'Memory',               'Phalcon\\Acl\\Adapter');
-    Alias::facade('AclRole',        'Role',                 'Phalcon\\Acl');
-    Alias::facade('AclResource',    'Resource',             'Phalcon\\Acl');
-    Alias::facade('ViewPhp',        'Php',                  'Phalcon\Mvc\View\Engine');
-    Alias::facade('ViewVolt',       'Volt',                 'Phalcon\Mvc\View\Engine');
-    Alias::facade('MVCDi',          'FactoryDefault',       'Phalcon\DI');
     Alias::facade('Datation',       'Carbon',               'Carbon');
     Alias::facade('I',              'Inflector',            'Thin');
     Alias::facade('A',              'Arrays',               'Thin');
@@ -66,7 +41,6 @@
     Alias::facade('Live',           'Boris',                'Boris');
     Alias::facade('Marksdown',      'CommonMarkConverter',  'League\CommonMark');
     Alias::facade('Crontab',        'CronExpression',       'Cron');
-    Alias::facade('Maily',          'Server',               'Ddeboer\Imap');
     Alias::facade('Client',         'Client',               'Goutte');
     Alias::facade('Finder',         'Finder',               'Symfony\Component\Finder');
     Alias::facade('Cloud',          'Store',                'S3');
